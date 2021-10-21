cb26bd6d70126600933567bbe4ee4ff90256a2f1
Yami3010 <yami8275@gmail.com>
2021-10-20 21:15:59 -0500

    Cambios (20/Oct/21): *Anexo y uso de fuente personalizada "Montserrat" *Mejora en los colores de los botones de navegación al mostrarse por primera vez *Implementación de un archivo PHP para enviar los datos del formulario de contacto. *Implementacion de instrucciones "action" y "method (POST)" en el formulario.

---
+++ enviar_formulario.php
@@ -0,0 +1,12 @@
+<?php
+
+//DECLARACION DE VARIABLES MEDIANTE POST
+$target = "yami8275@gmail.com"; //Correo al que se enviara el mensaje
+$username = $_POST['username'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+//Formato del mensaje
+$msg = "Nombre: ".$username."\n"."correo: ".$email."\n"."Mensaje: ".$mensaje;
+mail($target,"MENSAJE",$msg);
+header("location:index.html");
