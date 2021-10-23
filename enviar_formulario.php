adb5e383069683b5922f341beebcfd4c693bbb64
Yami3010 <yami8275@gmail.com>
2021-10-23 09:15:30 -0500

    Cambios (22/Oct/21): 1: Se agrego la "emergent_box" para dar un mensaje de agradecimiento por enviar el formulario. 2: Se configuro el FORMULARIO para enviar los datos mediante JAVASCRIPT al documento PHP, y con eso evitar el refresh de la pagina. 3: Se creo la FUNCION en Javascript "box_action()", la cual nos muestra el mensaje de agradecimiento con el envio de datos al doc PHP. 4: Creacion del documento PHP "enviar_formulario.php" el cual contiene la direccion de email al cual se enviarael correo y la formacion del correo.

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -2,12 +2,16 @@
 
 //DECLARACION DE VARIABLES MEDIANTE POST
 $target = "yami8275@gmail.com"; //Correo al que se enviara el mensaje
+
+//Variables obtenidas de la pagina web mediante Javascript.
 $username = $_POST['username'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
 
 //Formato del mensaje
-$msg = "Nombre: ".$username."\n"."correo: ".$email."\n"."Mensaje: ".$mensaje;
+$msg = "Hola, soy ".$username."."."\n\n".$mensaje."\n\n"."Puedes contactarme por siguiente correo: ".$email.".";
 mail($target,"MENSAJE",$msg);
+
 header("location:index.html");
+
 ?>
